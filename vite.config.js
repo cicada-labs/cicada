d99d9b14926bf4019bbf903af9d9505d51936478
akira-cn <akira.cn@gmail.com>
2023-07-20 14:15:56 +0800

    update build config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +15,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+       input: {
+          // 配置所有页面路径，使得所有页面都会被打包
+          main: resolve(__dirname, 'index.html'),
+          add: resolve(__dirname, 'add.html'),
+       }
+    }
+ }
 })
